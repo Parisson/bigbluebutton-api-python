b2808d6cec72e2515244734eb008690517120fa5
Yoan Le Clanche <yoanl@pilotsystems.net>
2020-05-07 15:42:33 +0200

    better handling of parameters with special characters

--- bigbluebutton_api_python/util/urlbuilder.py
+++ bigbluebutton_api_python/util/urlbuilder.py
@@ -1,6 +1,7 @@
 
 from hashlib import sha1
 from re import match
+import urllib
 
 class UrlBuilder:
     def __init__(self, bbbServerBaseUrl, securitySalt):
@@ -14,28 +15,30 @@ class UrlBuilder:
         self.securitySalt         = securitySalt
         self.bbbServerBaseUrl     = bbbServerBaseUrl
 
-    def buildUrl(self, api_call, params={}):
-        url = self.bbbServerBaseUrl
-        url += api_call + "?"
+    def buildQueryString(self, params):
+        sanitazed = {}
         for key, value in params.items():
+            if value is None:
+                continue
             if isinstance(value, bool):
                 value = "true" if value else "false"
             else:
-                value = str(value)
-            url += key + "=" + value + "&"
+                try:
+                    value = str(value)
+                except UnicodeEncodeError:
+                    value = value.encode('utf-8')
+            sanitazed[key] = value
+        return urllib.urlencode(sanitazed)
 
-        url += "checksum=" + self.__get_checksum(api_call, params)
+    def buildUrl(self, api_call, params={}):
+        url = self.bbbServerBaseUrl
+        url += api_call + "?"
+        url += self.buildQueryString(params)
+        url += "&checksum=" + self.__get_checksum(api_call, params)
         return url
 
     def __get_checksum(self, api_call, params={}):
         secret_str = api_call
-        for key, value in params.items():
-            if isinstance(value, bool):
-                value = "true" if value else "false"
-            else:
-                value = str(value)
-            secret_str += key + "=" + value + "&"
-        if secret_str.endswith("&"):
-            secret_str = secret_str[:-1]
+        secret_str += self.buildQueryString(params)        
         secret_str += self.securitySalt
-        return sha1(secret_str.encode('utf-8')).hexdigest()
+        return sha1(secret_str).hexdigest()
